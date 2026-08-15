aa99dfd903cbd40ca26fb26c87524a246712b99d
Oluwatobi Oluyede <oluwatobioluyede4@gmail.com>
2026-08-15 23:46:07 +0100

    fix(test): add conftest.py collect_ignore for slow test files
    pytest.ini --ignore paths are relative to the ini location, which breaks
    when pytest is invoked from a parent directory. conftest.py collect_ignore
    works from any invocation directory.

---
+++ packages/core-py/conftest.py
@@ -0,0 +1,7 @@
+collect_ignore = [
+    "tests/test_numpy_engine.py",
+    "tests/test_point_library.py",
+    "tests/test_safetensors_loader.py",
+    "tests/test_morph_tokenizer.py",
+    "tests/test_neural_e2e.py",
+]
