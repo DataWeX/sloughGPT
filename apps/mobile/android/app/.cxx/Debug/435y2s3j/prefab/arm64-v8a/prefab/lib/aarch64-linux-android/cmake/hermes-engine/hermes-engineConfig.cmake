bbe99bc1deee5edf79a0ad221660a077b5a70266
Oluwatobi Oluyede <oluwatobioluyede4@gmail.com>
2026-08-18 15:19:46 +0100

    gitignore: add Android build artifacts

--- apps/mobile/android/app/.cxx/Debug/435y2s3j/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::hermesvm)
-add_library(hermes-engine::hermesvm SHARED IMPORTED)
-set_target_properties(hermes-engine::hermesvm PROPERTIES
-    IMPORTED_LOCATION "/home/mana/.gradle/caches/8.13/transforms/582a79ea778f4033530662122e86dd0b/transformed/hermes-android-250829098.0.14-debug/prefab/modules/hermesvm/libs/android.arm64-v8a/libhermesvm.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/mana/.gradle/caches/8.13/transforms/582a79ea778f4033530662122e86dd0b/transformed/hermes-android-250829098.0.14-debug/prefab/modules/hermesvm/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
